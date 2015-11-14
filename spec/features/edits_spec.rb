810268cdc129e60a5bf095300225ce4725f8dcc8
Volney Tejeda <volneytejeda10@gmail.com>
2015-11-13 19:23:28 -0500

    edit happy path spec

---
+++ spec/features/edits_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper.rb'
+
+feature 'Creating, editing, and deleting a recipe', js: true do
+  scenario 'CRUD a recipe' do
+    visit '/'
+    click_on "New Recipe..."
+
+    fill_in "name", with: "Baked Brussel Sprouts"
+    fill_in "instructions", with: "Slather in oil, then bake for 20 minutes"
+
+    click_on "Save"
+
+    expect(page).to have_content("Baked Brussel Sprouts")
+    expect(page).to have_content("Slather in oil")
+
+    click_on "Edit"
+
+    fill_in "name", with: "Roasted Brussel Sprouts"
+    fill_in "instructions", with: "Slather in oil, then roast for 20 minutes"
+
+    click_on "Save"
+
+    expect(page).to have_content("Roasted Brussel Sprouts")
+    expect(page).to have_content("then roast for 20 minutes")
+
+    visit "/"
+    fill_in "keywords", with: "Roasted"
+    click_on "Search"
+
+    click_on "Roasted Brussel Sprouts"
+
+    click_on "Delete"
+
+    expect(Recipe.find_by_name("Roasted Brussel Sprouts").to be_nil)
+  end
+end
