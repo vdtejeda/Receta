d0cc48eea13dfc2f961f693f8dba88e45865ab21
Volney Tejeda <volneytejeda10@gmail.com>
2016-03-19 14:47:14 -0400

    add assets to rails precompile
    allows to teaspoon work properly

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,27 @@ Rails.application.config.assets.precompile += %w(
   teaspoon.css
   teaspoon-teaspoon.js
   teaspoon-jasmine.js
+  support/phantomjs-shims.self.js
+  support/bind-poly.self.js
+  jquery/dist/jquery.self.js
+  jquery_ujs.self.js
+  angular/angular.self.js
+  angular-route/angular-route.self.js
+  angular-resource/angular-resource.self.js
+  angular-rails-templates.self.js
+  angular-flash/dist/angular-flash.self.js
+  templates/form.self.js
+  templates/index.self.js
+  templates/show.self.js
+  app.self.js
+  controllers/RecipeController.self.js
+  controllers/RecipesController.self.js
+  home.self.js
+  application.self.js
+  angular-mocks/angular-mocks.self.js
+  spec_helper.self.js
+  controllers/RecipeController_spec.self.js
+  controllers/RecipesController_spec.self.js
 )
 
 # Add additional assets to the asset load path
